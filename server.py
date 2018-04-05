4a026b89ff07f80f760dbe90e2bf32376718ec5a
bogdanszonja <bogdanszonja@gmail.com>
2018-04-05 11:01:48 +0200

    the counts is now in dictionary and more methods added

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-COUNTS = 0
+COUNTS = {'GET': 0, 'POST': 0, 'DELETE': 0, 'PUT': 0}
 
 
-@app.route('/request_counter')
+@app.route('/request_counter', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def request_counter():
     global COUNTS
-    COUNTS += 1
+    for key in COUNTS.keys():
+        if request.method == key:
+            COUNTS[key] += 1
     return render_template("request_counter.html", counts=COUNTS)
 
 
